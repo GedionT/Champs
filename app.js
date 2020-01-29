ea9d5b4ab47ce64b9b5d47becc46a4521f089ee6
GedionT <teshomegedion@gmail.com>
2020-01-29 16:31:31 +0300

    added eslint configurations and dependencies

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var errorHandler = require('./services/errorHandler');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const errorHandler = require('./services/errorHandler');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//middleware
+// middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- config/db.js
+++ config/db.js
@@ -1,16 +1,20 @@
-const config = require('./config.json');
 const mongoose = require('mongoose');
+const config = require('./config.json');
+
+// eslint-disable-next-line max-len
 mongoose.connect(process.env.MONGODB_URI || config.connectionString, { userCreateIndex: true, userNewUrlParse: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 
 // Get current connected Database
-var db = mongoose.connection;
+const db = mongoose.connection;
 
-// Notify on error or succes
+// Notify on error or success
+// eslint-disable-next-line no-console
 db.on('error', console.error.bind(console, 'connection error: '));
+// eslint-disable-next-line no-console
 db.once('open', () => console.log('Connected to the Database'));
 
 module.exports = {
-    User    : require('../src/users/userModel'),
-    Items   : require('../src/items/itemModel')
-}
+  User: require('../src/users/userModel'),
+  Items: require('../src/items/itemModel')
+};
