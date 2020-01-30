6ab90a511e924108be2375d7cdb25ce7aa8d672a
GedionT <teshomegedion@gmail.com>
2020-01-30 10:56:59 +0300

    added userRoutes

---
+++ src/users/userRoute.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const userController = require('./userController');
+const auth = require('../../services/authMiddleware');
+
+router.post('/login', userController.login);
+router.post('/signup', userController.signup);
+router.get('/:id', auth, userController.getById);
+router.get('/', auth, userController.getAll);
+
+module.exports = router;
